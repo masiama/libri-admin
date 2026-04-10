10604f0ba65f026d95c9cecfa9584bb9648cd9ac
Leonid Cupikov <leonid.cupikov@gmail.com>
2026-04-10 20:48:02 +0200

    feat: add basic auth using supabase

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const logout = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) console.log(error)
+}
+</script>
+
+<template>
+  <template v-if="user">
+    <div>Logged in as {{ user.email }}</div>
+    <button @click="logout">Logout</button>
+  </template>
+  <template v-else>
+    <div>Not logged in</div>
+    <a href="/login">Log in</a>
+  </template>
+</template>
---
+++ app/pages/login.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const email = ref('')
+const { origin } = useRequestURL();
+
+const signInWithOtp = async () => {
+  console.log(process.env.BASE_URL);
+  const { error } = await supabase.auth.signInWithOtp({
+    email: email.value,
+    options: {
+      emailRedirectTo: `${origin}/confirm`,
+    }
+  })
+  if (error) console.log(error)
+}
+
+</script>
+<template>
+  <div>
+    <button @click="signInWithOtp">
+      Sign In with E-Mail
+    </button>
+    <input
+      v-model="email"
+      type="email"
+    />
+  </div>
+</template>
