037a3aaecb23bf1da0156ae9360efdb54c19e5e0
Leonid Cupikov <leonid.cupikov@gmail.com>
2026-04-12 20:10:05 +0200

    refactor: update login page ui

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,28 +1,55 @@
 <script setup lang="ts">
-const supabase = useSupabaseClient();
-const email = ref("");
-const { origin } = useRequestURL();
+import type { AuthFormField, FormSubmitEvent } from "@nuxt/ui";
 
-const signInWithOtp = async () => {
-  console.log(process.env.BASE_URL);
+const supabase = useSupabaseClient();
+const { origin } = useRequestURL();
+const toast = useToast();
+
+const loading = ref(false);
+const invalid = ref(false);
+
+const onSubmit = async (payload: FormSubmitEvent<{ email: string }>) => {
+  invalid.value = false;
+  loading.value = true;
+
   const { error } = await supabase.auth.signInWithOtp({
-    email: email.value,
-    options: {
-      emailRedirectTo: `${origin}/confirm`,
-    },
+    email: payload.data.email,
+    options: { emailRedirectTo: `${origin}/confirm` },
   });
-  if (error) console.log(error);
+
+  loading.value = false;
+
+  if (error) {
+    invalid.value = true;
+    console.log(error);
+  } else {
+    toast.add({
+      title: "Check your email",
+      description: "We've sent you a magic link to sign in. Please check your inbox.",
+      progress: false,
+    });
+  }
 };
+
+const fields: AuthFormField[] = [
+  { name: "email", type: "email", label: "Email", placeholder: "Enter your email", required: true },
+];
 </script>
+
 <template>
-  <UContainer class="flex h-screen items-center justify-center">
-    <UForm class="flex flex-col space-y-4 rounded border border-white/20 bg-white/5 p-4">
-      <UFormField label="Email" name="email">
-        <UInput v-model="email" placeholder="Enter your email" />
-      </UFormField>
-      <UButton icon="i-lucide-user" variant="subtle" @click="signInWithOtp">
-        Sign In with E-Mail
-      </UButton>
-    </UForm>
-  </UContainer>
+  <div class="flex h-screen flex-col items-center justify-center gap-4 p-4">
+    <UPageCard class="w-full max-w-md" variant="subtle">
+      <UAuthForm
+        :fields="fields"
+        title="Welcome back!"
+        icon="i-lucide-lock"
+        @submit="onSubmit"
+        :loading="loading"
+      >
+        <template #validation v-if="invalid">
+          <UAlert color="error" icon="i-lucide-info" title="Error signing in" />
+        </template>
+      </UAuthForm>
+    </UPageCard>
+  </div>
 </template>
