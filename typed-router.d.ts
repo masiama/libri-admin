51580d610bc4e365145c394b3876b4f693228943
Leonid Cupikov <leonid.cupikov@gmail.com>
2026-05-28 07:58:47 +0200

    chore: update deps

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -14,11 +14,15 @@ import type {
   ParamValueZeroOrMore,
   ParamValueZeroOrOne,
 } from 'vue-router'
+import type {
+  _ExtractParamParserType,
+} from 'vue-router/experimental'
 
 declare module 'vue-router' {
   interface TypesConfig {
-    ParamParsers:
-      | never
+    _ParamParsers: {}
+    RouteNamedMap: import('vue-router/auto-routes').RouteNamedMap
+    _RouteFileInfoMap: import('vue-router/auto-routes')._RouteFileInfoMap
   }
 }
 
@@ -73,11 +77,15 @@ declare module 'vue-router/auto-routes' {
         | '/'
       views:
         | never
+      pathParamNames:
+        | never
     }
     'src/pages/crawler.vue': {
       routes:
         | '/crawler'
       views:
+        | never
+      pathParamNames:
         | never
     }
     'src/pages/login.vue': {
@@ -85,11 +93,15 @@ declare module 'vue-router/auto-routes' {
         | '/login'
       views:
         | never
+      pathParamNames:
+        | never
     }
     'src/pages/purgatory.vue': {
       routes:
         | '/purgatory'
       views:
+        | never
+      pathParamNames:
         | never
     }
   }
